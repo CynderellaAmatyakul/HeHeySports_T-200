7616481ff54dd2984ac049a3e2858b39cb6e8069
Cynderella <37022chayanin@gmail.com>
2024-02-27 02:58:11 +0700

    Timer

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CountDownTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] float remainingTime;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (remainingTime >= 0)
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
