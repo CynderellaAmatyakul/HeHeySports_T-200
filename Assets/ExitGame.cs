6c8f8bd89c6105776684bec16aa89a64bf7741f9
113ulo <mekmingming@gmail.com>
2024-03-16 07:45:36 +0700

    Final_Menu

--- Assets/ExitGame.cs
+++ Assets/ExitGame.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Play : MonoBehaviour
+public class ExitGame : MonoBehaviour
 {
-   public void LoadScene(string sceneName)
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-    void Start()
+    public void Exit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
