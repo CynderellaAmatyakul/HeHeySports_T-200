beccfe11622a49ad9269a2a8093f821b0c9a9999
Cynderella <37022chayanin@gmail.com>
2024-03-16 08:03:29 +0700

    fixed bug

--- Assets/Scripts/Puzzle2Controller.cs
+++ Assets/Scripts/Puzzle2Controller.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Puzzle2Controller : MonoBehaviour
 {
