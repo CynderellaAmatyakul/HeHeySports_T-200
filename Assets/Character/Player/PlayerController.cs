46769be0d0ddb2de4f11cfc5024ae1056d60a763
Cynderella <37022chayanin@gmail.com>
2024-04-04 01:38:29 +0700

    fixed bug wall

--- Assets/Character/Player/PlayerController.cs
+++ Assets/Character/Player/PlayerController.cs
@@ -12,15 +12,52 @@ public class PlayerController : MonoBehaviour
 
     public Animator animator;
 
+    public bool stopRight;
+    public bool stopLeft;
+    public bool stopUp;
+    public bool stopDown;
+
+    private void FixedUpdate()
+    {
+        stopRight = Physics.Raycast(transform.position, Vector3.right, 0.67f);
+        stopLeft = Physics.Raycast(transform.position, Vector3.left, 0.67f);
+        stopUp = Physics.Raycast(transform.position, Vector3.up, 0.67f);
+        stopDown = Physics.Raycast(transform.position, Vector3.down, 0.67f);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        //if (!stopRight)
+        //{
+        //    moveRight(move);
+        //}
+
+        //if (!stopLeft)
+        //{
+        //    moveLeft(move);
+        //}
+
+        //if (!stopUp)
+        //{
+        //    moveUp(move);
+        //}
+
+        //if (!stopDown)
+        //{
+        //    moveDown(move);
+        //}
+
+        //if (!stopRight || !stopLeft || !stopUp || !stopDown)
+        //{
+        //    movePlayer();
+        //}
+
         movePlayer();
 
         animator.SetFloat("move.x", move.x);
         animator.SetFloat("move.y", move.y);
         animator.SetFloat("speed", speed);
-
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -35,5 +72,53 @@ public class PlayerController : MonoBehaviour
         //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    private void moveRight(Vector2 movement)
+    {
+        if (movement.x > 0)
+        {
+            movement = new Vector3(move.x, 0f, move.y);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void moveLeft(Vector2 movement)
+    {
+        if (movement.x < 0)
+        {
+            movement = new Vector3(move.x, 0f, move.y);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void moveUp(Vector2 movement)
+    {
+        if (movement.y > 0)
+        {
+            movement = new Vector3(move.x, 0f, move.y);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void moveDown(Vector2 movement)
+    {
+        if (movement.y < 0)
+        {
+            movement = new Vector3(move.x, 0f, move.y);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
     }
 }
